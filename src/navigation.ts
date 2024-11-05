0cbd80b10d73eb72f625c847a5563937ac65aa40
Nicholas Nadeau <nicholas.nadeau@gmail.com>
2024-11-05 11:55:04 -0500

    --wip-- [skip ci]

---
+++ src/navigation.ts
@@ -0,0 +1,14 @@
+import { getAsset } from './utils/permalinks';
+
+export const headerData = {
+  links: [],
+};
+
+export const footerData = {
+  links: [],
+  secondaryLinks: [],
+  socialLinks: [
+    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
+    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/engnadeau' },
+  ],
+};
